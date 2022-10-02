c590f5af06d5c020cae368d7c6fc088e5458a517
brnkgabriel <brnkgabriel@gmail.com>
2022-10-02 15:24:35 +0100

    auth: added firebase

--- app.vue
+++ app.vue
@@ -3,11 +3,10 @@
     <h2>Hello from Nuxt 3</h2>
     <h2>{{double}}</h2>
     <h1>{{count}}</h1>
+    <input type="date" name="birthday" id="birthday"/>
   </div>
 </template>
 <script setup>
-  import { ref, computed } from "#imports"
-import Welcome from "./components/Welcome.vue";
   // ref() and computed() are auto-imported
   const count = ref(1)
   const double = computed(() => count.value * 8)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  buildModules:['@nuxtjs/tailwindcss']
+  buildModules:['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    apiSecret: '', // can be overridden by NUXT_API_SECRET environment variable
+    public: {
+      FB_API_KEY: process.env.FB_API_KEY,
+      FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
+      FB_DATABASE_URL: process.env.FB_DATABASE_URL,
+      FB_PROJECT_ID: process.env.FB_PROJECT_ID,
+      FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET,
+      FB_MESSAGING_SENDER_ID: process.env.FB_MESSAGING_SENDER_ID,
+      FB_APP_ID: process.env.FB_APP_ID,
+      FB_MEASUREMENT_ID: process.env.FB_MEASUREMENT_ID
+    }
+  },
 })
