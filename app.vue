427a3a920b3a1f92da0c4449e2465d1cf70e6823
brnkgabriel <brnkgabriel@gmail.com>
2022-10-02 14:22:57 +0100

    ci: initial

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-slate-900 h-screen">
+    <div class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-slate-9">
+      <div class="max-w-md w-full space-y-8">
+        <div>
+          <div class="h-25 w-25"></div>
+          <img src="/img/logo_short.png" alt="full stack logo" class="mx-auto h-48 w-auto"/>
+          <h2>
+            <span>
+              Check out other
+            </span>
+            <span class="text-sky-500">Full stack Jack videos on</span>
+            <span class="text-lime-500">Nuxt</span>
+          </h2>
+          <p class="mt-2 text-center text-sm text-gray-600"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+  import { ref, computed } from "#imports"
+import Welcome from "./components/Welcome.vue";
+  // ref() and computed() are auto-imported
+  const count = ref(1)
+  const double = computed(() => count.value * 8)
+</script>
---
+++ components/Welcome.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <NuxtLogo />
+    <h1>Welcome to Nuxt</h1>
+  </div>
+</template>
+<script>
+import NuxtLogo from './NuxtLogo.vue';
+export default {
+    components: { NuxtLogo }
+}
+</script>
+<style>
+  body {
+    margin: 0;
+  }
+  .container {
+    height: 100vh;
+    width: 100vw;
+    background: rgb(1, 42, 53);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  h1 {
+    color: white;
+    text-align: center;
+    font-family: sans-serif;
+    font-weight: 400;
+  }
+</style>
