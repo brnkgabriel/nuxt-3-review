6f8f06de33d9cb5385436a82b66aeeafd96e6e65
brnkgabriel <brnkgabriel@gmail.com>
2022-10-03 03:14:06 +0100

    feat: display credentials

--- app.vue
+++ app.vue
@@ -6,7 +6,9 @@
     <!-- <Forms /> -->
     <!-- <PageHeadings /> -->
     <!-- <InputGroups /> -->
-    <Login />
+    <!-- <Login /> -->
+    <button @click="signIn">Sign in</button>
+    <button @click="signOut">Sign out</button>
     <!-- <HeroSection /> -->
     <!-- <FeatureSection /> -->
     <!-- <CTASection /> -->
@@ -28,16 +30,30 @@
     <!-- <ProductFeatures /> -->
     <!-- <StoreNavigation /> -->
     <!-- <PromoSections /> -->
+    <pre>
+      {{credentials}}
+    </pre>
   </div>
 </template>
 <script setup>
-  // ref() and computed() are auto-imported
-  const count = ref(1)
-  const double = computed(() => count.value * 8)
+
+const credentials = ref()
+
+const signIn = async () => {
+
+  const email = "brnkgabriel@gmail.com";
+  const password = "1234567";
+  credentials.value = await signInUser(email, password)
+};
+
+const signOut = async () => {
+  credentials.value = await signOutUser();
+};
+
 </script>
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-  body {
-    font-family: Poppins, "DM Sans";
-  }
+@import url("https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+body {
+  font-family: Poppins, "DM Sans";
+}
 </style>
